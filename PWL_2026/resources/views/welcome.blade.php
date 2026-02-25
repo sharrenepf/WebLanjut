40681cff39d8293e8075bb5d5acb8ff994d94c61
Sharren <sharrenepf@gmail.com>
2026-02-25 12:50:28 +0700

    notifikasi

--- PWL_2026/resources/views/welcome.blade.php
+++ PWL_2026/resources/views/welcome.blade.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>PWL 2024</title>
+    <title>PWL 2026</title>
 </head>
 <body>
-    <h2>Pemrograman Web Lanjut 2024</h2>
+    <h2>Pemrograman Web Lanjut 2026</h2>
     <h3>Semangat Belajar Laravel 10</h3>
 </body>
 </html>
